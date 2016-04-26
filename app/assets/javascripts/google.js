bb239668fc4bfdb909f05e81f7b77536ae61020a
Shraddha Patel <spatelrb@gmail.com>
2016-04-25 23:41:36 -0400

    changed styling

--- app/assets/javascripts/google.js
+++ app/assets/javascripts/google.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
   var infowindow = new google.maps.InfoWindow(); 
   var marker;
   var polyline = null;
+  var lat;
+  var lng;
+  var mlat = {};
 
   //AUTOCOMPLETE ADDRESS FOR FORM ON INITIAL LOAD
   google.maps.event.addDomListener(window, 'load', initialize);
@@ -64,62 +67,10 @@ $(document).ready(function() {
 
   //SHOWS INFOWINDOW FOR SEARCH 
   function bindInfoWindow(marker, map, infowindow, content) {
-  marker.addListener('click', function(){
-    infowindow.setContent(content);  
-    infowindow.open(map, this);           
-  }); 
-  } 
-
-  //SHOWS MAP
-  function initMap() {
-    var mySLatLng = new google.maps.LatLng(40.708445, -74.005666);
-    var myOptions = {
-      zoom: 12,
-      center: mySLatLng
-    }
-    
-    map = new google.maps.Map(document.getElementById('map'), myOptions);
-    polyline = new google.maps.Polyline({
-      path: []
-    });
-    directionsDisplay.setMap(map);
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-
-    var request = {
-      location: mySLatLng,
-      radius: '500',
-      types: ['restaurant']
-    };
-
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, function(results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          var place = results[i];
-          var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
-          });
- 
-          var content = '<strong>' + place.name + '</strong><br>' 
-            + '$$$ = ' + place.price_level + '<br>' 
-            // + 'Place ID: ' + place.place_id + '<br>' // Test tag, unnecessary for users
-            + 'Address: ' + place.vicinity + '<br>'  //using "place.formatted_address" would come up as undefined
-            // + 'Ph: ' + place.formatted_phone_number + '<br>' //also undefined 
-            + 'Rating: ' + place.rating + ' out of 5 stars' + '<br>'  
-            + 'Coordinates: ' + place.geometry.location + '<br>'
-            + 'Is it open now? ' + place.opening_hours.open_now + '<br>'
-        
-          bindInfoWindow(marker, map, infowindow, content) 
-        }
-      }
-    });
-
-  //MODE OF TRANSPORT REFLECTED ON CHANGE INSTEAD OF HAVING TO PRESS REFRESH
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-    document.getElementById('mode').addEventListener('change', function() {
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
-    });
+    marker.addListener('click', function(){
+      infowindow.setContent(content);  
+      infowindow.open(map, this);
+    })         
   }
 
   //TAKES START AND END LOCATIONS AND RENDERS DIRECTION ON MAP
@@ -142,8 +93,6 @@ $(document).ready(function() {
         endLocation = new Object();
         directionsDisplay.setDirections(response);
         var route = response.routes[0];
-        // var summaryPanel = document.getElementById('directions_panel');
-        // summaryPanel.innerHTML = "";
 
         // For each route, display summary information.
         var path = response.routes[0].overview_path;
@@ -168,7 +117,6 @@ $(document).ready(function() {
             }
 
         polyline.setMap(map);
-        console.log()
         computeTotalDistance(response);
       } else {
         alert('directions response ' + status);
@@ -187,8 +135,8 @@ $(document).ready(function() {
           totalTime += myroute.legs[i].duration.value;      
         }
         putMarkerOnRoute(50);
-        console.log(totalDist)
-        console.log(totalTime)
+        // console.log(totalDist)
+        // console.log(totalTime)
       }
 
       function putMarkerOnRoute(percentage) {
@@ -199,16 +147,118 @@ $(document).ready(function() {
         } else {
           marker.setPosition(polyline.GetPointAtDistance(distance));
           marker.setTitle('time:' + time);
-          console.log(marker.getPosition().lat())
-          console.log(marker.getPosition().lng())
+          // console.log(marker.getPosition().lat())
+          // console.log(marker.getPosition().lng())
+        }
+        var lat = marker.getPosition().lat()
+        var lng = marker.getPosition().lng()
+        console.log(lat)
+        console.log(lng)
+
+        mlat["lat"] = lat;
+        mlat["lng"] = lng;
+        console.log(mlat);
+        // var mlat = {"lat" : lat, "lng" : lng};
+
+      }
+
+  //SHOWS MAP
+  function initMap() {
+
+ // var styles = [
+ //    {
+ //      stylers: [
+ //        { hue: "#00ffe6" },
+ //        { saturation: -20 }
+ //      ]
+ //    },{
+ //      featureType: "road",
+ //      elementType: "geometry",
+ //      stylers: [
+ //        { lightness: 100 },
+ //        { visibility: "simplified" }
+ //      ]
+ //    },{
+ //      featureType: "road",
+ //      elementType: "labels",
+ //      stylers: [
+ //        { visibility: "off" }
+ //      ]
+ //    }
+ //  ];
+
+  // var styledMap = new google.maps.StyledMapType(styles,
+  //   {name: "Styled Map"});
+
+    var mySLatLng = new google.maps.LatLng(mlat.lat, mlat.lng);
+    // var mySLatLng = new google.maps.LatLng(40.72413068375255, -73.9732228774983);
+    // var mySLatLng = new google.maps.LatLng(40.75, -73.98383);
+    // console.log(mySLatLng)
+    var myOptions = {
+      zoom: 12,
+      center: mySLatLng
+    //   mapTypeControlOptions: {
+    //   mapTypeId: [google.maps.mapTypeId.ROADMAP, 'map_style']
+    // }
+    };
+    
+    map = new google.maps.Map(document.getElementById('map'), myOptions);
+      // map.mapTypes.set('map_style', styledMap);
+      // map.setMapTypeId('map_style');
+    polyline = new google.maps.Polyline({
+      path: []
+    });
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+    var request = {
+      location: mySLatLng,
+      radius: '500',
+      types: ['restaurant']
+    };
+
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, function(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+          });
+ 
+          var content = '<strong>' + place.name + '</strong><br>' 
+            + '<strong>' + 'Address: ' + '</strong>' + place.vicinity + '<br>'  //using "place.formatted_address" would come up as undefined
+            + '<strong>' + 'Is it open now? ' + '</strong>' + place.opening_hours.open_now + '<br>'
+            + '<strong>' + 'Rating: ' + '</strong>' + place.rating + ' out of 5 stars' + '<br>'  
+            + '<strong>' + '$$$ = ' + '</strong>' + place.price_level + '<br>' 
+            // + 'Place ID: ' + place.place_id + '<br>' // Test tag, unnecessary for users
+            // + 'Ph: ' + place.formatted_phone_number + '<br>' //also undefined 
+            // + 'Coordinates: ' + place.geometry.location + '<br>' // Test tag, unnecessary for users
+        
+          bindInfoWindow(marker, map, infowindow, content) 
         }
       }
+    });
+  }
+
+  //MODE OF TRANSPORT REFLECTED ON CHANGE INSTEAD OF HAVING TO PRESS REFRESH
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+  document.getElementById('mode').addEventListener('change', function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  });
+
+    // calculateAndDisplayRoute(directionsService, directionsDisplay);
+    // document.getElementById('mode').addEventListener('change', function() {
+    //   calculateAndDisplayRoute(directionsService, directionsDisplay);
+    // });
 
   //FUNCTIONS THAT ONLY HIT ON SUBMIT
   $('#submit_button').click(function(){
+    initMap();
     calculateAndDisplayRoute(directionsService, directionsDisplay);
-    initMap()
     // searchAddress()
     });
+
 });
 
